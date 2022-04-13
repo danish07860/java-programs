1c0a3368373ea00f96f55bc4f5c77845841492a5
danish07860 <mddanishalauddin018@gmail.com>
2022-04-14 04:31:13 +0530

    java program

---
+++ java programming/src/com/practice/numbers/Swap.java
@@ -0,0 +1,13 @@
+package com.practice.numbers;
+
+public class Swap {
+	public static void main(String[] args) {
+		int x=10,y=8;
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
